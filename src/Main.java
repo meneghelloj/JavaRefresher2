2211162e306db74099495889ae90d434810d484b
Josh <meneghelloj@yahoo.com>
2019-09-25 21:47:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args){
+    //System.out.println(recurseMe(1,true));
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Joe");
+        names.add("Bob");
+        names.add("Billy");
+        System.out.println(recurseMe(names.get(0),0,names));
+    }
+
+
+    public static String recurseMe(String name, int i, ArrayList<String> names){
+        System.out.println(name);
+        if (i == names.size()-1){
+            return "That's all folks!";
+        }else {
+            return recurseMe(names.get(i+1)+ " "+ name,i+1,  names);
+        }
+
+
+    }
+}
