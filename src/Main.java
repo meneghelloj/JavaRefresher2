dfea7a8401cb1780eab6a40c79f1ad8e96007012
Josh <meneghelloj@yahoo.com>
2019-09-25 21:52:17 -0400

    Iteration + Recursion because why not

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ public class Main {
 
     public static void main(String[] args){
     //System.out.println(recurseMe(1,true));
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<>();
         names.add("Joe");
         names.add("Bob");
         names.add("Billy");
-        System.out.println(recurseMe(names.get(0),0,names));
+        for(int i=0; i < names.size();i++) {
+            System.out.println(recurseMe(names.get(i), i, names));
+        }
     }
 
 
-    public static String recurseMe(String name, int i, ArrayList<String> names){
+    private static String recurseMe(String name, int i, ArrayList<String> names){
         System.out.println(name);
         if (i == names.size()-1){
             return "That's all folks!";
